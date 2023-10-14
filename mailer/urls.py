e0c3dd61a5757e08b682639755a0379a2a26e9e9
ShadeSWD <seawolfy60@gmail.com>
2023-10-14 11:30:51 +0300

    mailing detail

--- mailer/urls.py
+++ mailer/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from mailer.views import MailingsListView, MailingDetailView
 
 app_name = 'mailer'
 
 urlpatterns = [
+    path('', MailingsListView.as_view(), name='mailings_list'),
+    path('mailing/<int:pk>', MailingDetailView.as_view(), name='mailing_detail'),
 ]
--- mailer/views.py
+++ mailer/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from mailer.models import *
 
-# Create your views here.
+
+class MailingsListView(ListView):
+    model = Mailing
+    template_name = "mailer/mailings.html"
+    context_object_name = 'mailings'
+    queryset = Mailing.objects.all()
+
+
+class MailingDetailView(DetailView):
+    model = Mailing
+    template_name = "mailer/mailing.html"
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['mailing'] = Mailing.objects.get(pk=self.object.pk)
+        context_data['recipients'] = Mailing.objects.get(pk=self.object.pk).recipients.all()
+        return context_data
